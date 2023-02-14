c7dbb5e337885bf6c8352c4bf9855d7b94dbbf9a
m <aku_soku_zan_1bantai@hotmail.com>
2023-02-14 17:47:45 +0900

    壊せる壁を実装

--- DAF_Script/Dungeon/BreakObject.cs
+++ DAF_Script/Dungeon/BreakObject.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class BreakObject : MonoBehaviour
 {
+    public int hitCount = 1;
+    public string SWITCH_KEY = "";
+    public string SWITCH_VALUE = "";
     public GameObject PEACE_PREFAB;
     public GameObject BASE_PREFAB;
     public AudioClip BREAK_SE;
@@ -13,9 +16,12 @@ public class BreakObject : MonoBehaviour
     public float item_base_y = 0.5f;
     public int peaseNum = 5;
     public float peaseRandomRange = 0.3f;
+
     bool hasBroken = false;
 
     float MIN_BREAK_IMPACT = 1.0f;
+
+    string HIT_SE = "HitObjectSe";
 
     // Start is called before the first frame update
     void Start()
@@ -35,32 +41,67 @@ public class BreakObject : MonoBehaviour
             //インパクトが小さかったら壊れない
             if (impact < MIN_BREAK_IMPACT) return;
 
-            hasBroken = true;
+            hitCount--;
             ContactPoint contact = collision.contacts[0];
-            GameObject dungeonRoot = GetDungeonRootObject();
+            MakeHitEffect(contact);
+            MakeHitSE(HIT_SE);
+            if (hitCount <= 0) Break(impact, contact);
+            
+        }
+    }
 
-            MakeItem(ITEM_PREFAB, ITEM_PROBABILITY, item_base_y) ;
+    void MakeHitEffect(ContactPoint contact) {
+        Quaternion r = SingletonGeneral.instance.GetQuaternionFace();
+        //ダメージエフェクト
+        GameObject hit = Instantiate(
+            PlayerView.instance.config.PUNCH_HIT_PREFAB,
+            contact.point,
+            r);
+    }
+    void MakeHitSE(string SeName) {
+        //ダメージ音
+        SingletonGeneral.instance.PlayOneShotNoAudioSource(SeName);
+    }
+
+    void Break(float impact, ContactPoint contact) {
+        hasBroken = true;
+        
+        GameObject dungeonRoot = GetDungeonRootObject();
+
+        MakeItem(ITEM_PREFAB, ITEM_PROBABILITY, item_base_y);
+        //BasePrefabが設定されているときはbase objectを作る
+        if(BASE_PREFAB != null) {
             GameObject baseObject = MakeBaseObject(
-                BASE_PREFAB, 
-                gameObject.transform.position, 
+                BASE_PREFAB,
+                gameObject.transform.position,
                 gameObject.transform.rotation,
                 dungeonRoot);
-            
-            MakePeaceObjects(
-                PEACE_PREFAB,
-                contact,
-                gameObject.transform.rotation,
-                impact,
-                peaseNum,
-                peaseRandomRange,
-                dungeonRoot
-                );
-            
             //元のオブジェクトは消すので、残るオブジェクトで音を鳴らす
             baseObject.GetComponent<AudioSource>().PlayOneShot(BREAK_SE);
-            
-            Destroy(gameObject);
         }
+        else{
+            //残るオブジェクトがない場合はシングルトンの方でSEを鳴らす
+            SingletonGeneral.instance.PlayOneShotNoAudioWithClip(BREAK_SE);
+        }
+        
+        MakePeaceObjects(
+            PEACE_PREFAB,
+            contact,
+            gameObject.transform.rotation,
+            impact,
+            peaseNum,
+            peaseRandomRange,
+            dungeonRoot
+            );
+
+
+        SetSwitch();
+        Destroy(gameObject);
+    }
+
+    void SetSwitch() {
+        if (SWITCH_KEY == "") return;
+        SingletonGeneral.instance.scenarioSystem.SetSwitch(SWITCH_KEY, SWITCH_VALUE);
     }
 
     void MakePeaceObjects(
